4f403a8cf0d95e5ab61f0e4fad7f9fac2050ccfb
igorbugaiov <proxipixel@gmail.com>
2023-04-11 20:35:49 +0200

    st1-mod1-final 1st try

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String[] arr = getDataValues(getRawDataEntries(file));
+
+        return new Profile(
+                arr[0],
+                Integer.parseInt(arr[1]),
+                arr[2],
+                Long.decode(arr[3]));
+    }
+
+    private String[] getRawDataEntries(File dataFile) {
+        String[] arr = null;
+
+        try (BufferedReader r = new BufferedReader(new java.io.FileReader(dataFile))) {
+            arr = r.lines().toArray(String[]::new);
+        } catch (IOException ex) {
+            System.out.println("mapping went awry!");
+        }
+
+        return arr;
+    }
+
+    private String[] getDataValues(String[] data) {
+        String[] arr = new String[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            String s = data[i];
+            arr[i] = s.substring(s.indexOf(' ') + 1);
+        }
+        return arr;
     }
 }
